1191b00b7c5771646e93429a2e4e09d8219255b7
Audun Halland <audun.halland@gmail.com>
2019-05-04 22:02:42 +0200

    Fix hyper_request_to_pact_request

--- rust/pact_mock_server_async/src/server.rs
+++ rust/pact_mock_server_async/src/server.rs
@@ -8,8 +8,14 @@ use log::{log, debug, warn};
 use hyper::{Body, Response, Server, Error};
 use hyper::header::ToStrError;
 use hyper::service::service_fn_ok;
+use futures::future;
 use futures::future::Future;
 use futures::stream::Stream;
+
+enum MockRequestError {
+    InvalidHeaderEncoding,
+    RequestBodyError
+}
 
 fn extract_path(uri: &hyper::Uri) -> String {
     uri.path_and_query()
@@ -24,20 +30,26 @@ fn extract_query_string(uri: &hyper::Uri) -> Option<HashMap<String, Vec<String>>
         .and_then(|query| parse_query_string(&query.into()))
 }
 
-fn extract_headers(headers: &hyper::HeaderMap) -> Result<Option<HashMap<String, String>>, ToStrError> {
+fn extract_headers(headers: &hyper::HeaderMap) -> Result<Option<HashMap<String, String>>, MockRequestError> {
     if headers.len() > 0 {
-        let result: Result<HashMap<String, String>, ToStrError> = headers.keys()
-            .map(|name| -> Result<(String, String), ToStrError> {
+        let result: Result<HashMap<String, String>, MockRequestError> = headers.keys()
+            .map(|name| -> Result<(String, String), MockRequestError> {
                 let values = headers.get_all(name);
                 let mut iter = values.iter();
 
                 let first_value = iter.next().unwrap();
 
                 if iter.next().is_some() {
-                    warn!("Multiple headers associated with '{}', but only the first is matched", name);
+                    warn!("Multiple headers associated with '{}', but only the first is used", name);
                 }
 
-                Ok((name.as_str().into(), first_value.to_str()?.into()))
+                Ok((
+                    name.as_str().into(),
+                    first_value.to_str()
+                        .map_err(|err| MockRequestError::InvalidHeaderEncoding)?
+                        .into()
+                    )
+                )
             })
             .collect();
 
@@ -56,23 +68,30 @@ pub fn extract_body(chunk: hyper::Chunk) -> OptionalBody {
     }
 }
 
-fn hyper_request_to_pact_request(req: hyper::Request<Body>) -> impl Future<Item = Request, Error = hyper::Error> {
+fn hyper_request_to_pact_request(req: hyper::Request<Body>) -> impl Future<Item = Request, Error = MockRequestError> {
     let method = req.method().to_string();
     let path = extract_path(req.uri());
     let query = extract_query_string(req.uri());
     let headers = extract_headers(req.headers());
 
-    req.into_body()
-        .concat2()
-        .and_then(|body_chunk| Ok(Request {
-            method: req.method().to_string(),
-            path: path,
-            query: query,
-            headers: headers,
-            body: extract_body(body_chunk),
-            matching_rules: MatchingRules::default(),
-            generators: Generators::default()
-        }))
+    future::done(headers)
+        .and_then(move |headers| {
+            req.into_body()
+                .concat2()
+                .map_err(|_| MockRequestError::RequestBodyError)
+                .map(|body_chunk| (headers, body_chunk))
+        })
+        .and_then(|(headers, body_chunk)|
+            Ok(Request {
+                method: method,
+                path: path,
+                query: query,
+                headers: headers,
+                body: extract_body(body_chunk),
+                matching_rules: MatchingRules::default(),
+                generators: Generators::default()
+            })
+        )
 }
 
 pub fn start(
