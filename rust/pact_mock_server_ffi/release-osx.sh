37b10835bd0c006b4afb8477824503faedf9531c
Ronald Holshausen <uglyog@gmail.com>
2019-09-07 14:25:16 +1000

    chore: update release scripts to cross compile

--- rust/pact_mock_server_ffi/release-osx.sh
+++ rust/pact_mock_server_ffi/release-osx.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 if [ "" = "$1" ]; then
   echo "Usage: "
@@ -6,10 +6,16 @@ if [ "" = "$1" ]; then
   exit 1
 fi
 
+if [ "" = "$OSXCROSS" ]; then
+  echo "This script needs OSXCROSS set to the home of osxcross"
+  exit 1
+fi
+
+export PATH="$OSXCROSS/target/bin:$PATH"
+export CC=o64-clang
+export CXX=o64-clang++
+
 cargo clean
-cargo build --release
-cargo build --release --target x86_64-apple-ios
-gzip -c ../target/release/libpact_mock_server_ffi.dylib > ../target/release/libpact_mock_server_ffi-osx-x86_64-$1.dylib.gz
-gzip -c ../target/release/libpact_mock_server_ffi.a > ../target/release/libpact_mock_server_ffi-osx-x86_64-$1.a.gz
-cargo build --release --target x86_64-apple-ios
-gzip -c ../target/x86_64-apple-ios/release/libpact_mock_server_ffi.a > ../target/x86_64-apple-ios/release/libpact_mock_server_ffi-ios-x86_64-$1.a.gz
+cargo build --release --target x86_64-apple-darwin
+gzip -c ../target/x86_64-apple-darwin/release/libpact_mock_server_ffi.dylib > ../target/release/libpact_mock_server_ffi-osx-x86_64-$1.dylib.gz
+gzip -c ../target/x86_64-apple-darwin/release/libpact_mock_server_ffi.a > ../target/release/libpact_mock_server_ffi-osx-x86_64-$1.a.gz
--- rust/pact_mock_server_ffi/release-win.sh
+++ rust/pact_mock_server_ffi/release-win.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ "" = "$1" ]; then
   echo "Usage: "
@@ -7,6 +7,6 @@ if [ "" = "$1" ]; then
 fi
 
 cargo clean
-cargo build --release
-gzip -c ../target/release/pact_mock_server_ffi.dll > ../target/release/pact_mock_server_ffi-windows-x86_64-$1.dll.gz
-gzip -c ../target/release/pact_mock_server_ffi.lib > ../target/release/pact_mock_server_ffi-windows-x86_64-$1.lib.gz
+cargo build --release --target x86_64-pc-windows-gnu
+gzip -c ../target/x86_64-pc-windows-gnu/release/pact_mock_server_ffi.dll > ../target/release/libpact_mock_server_ffi-windows-x86_64-$1.dll.gz
+gzip -c ../target/x86_64-pc-windows-gnu/release/pact_mock_server_ffi.lib > ../target/release/libpact_mock_server_ffi-windows-x86_64-$1.lib.gz
