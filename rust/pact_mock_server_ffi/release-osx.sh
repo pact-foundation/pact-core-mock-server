e2b10ef0d3ac9a5d2d99e26992c21e5a843629dc
Ronald Holshausen <uglyog@gmail.com>
2020-04-24 13:25:33 +1000

    chore: debugging github release action

--- rust/pact_mock_server_ffi/release-osx.sh
+++ rust/pact_mock_server_ffi/release-osx.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -e
 
-#if [ "" = "$OSXCROSS" ]; then
-#  echo "This script needs OSXCROSS set to the home of osxcross"
-#  exit 1
-#fi
-#
-#export PATH="$OSXCROSS/target/bin:$PATH"
-#export CC=o64-clang
-#export CXX=o64-clang++
-
 cargo clean
 cargo build --release
-gzip -c ../target/x86_64-apple-darwin/release/libpact_mock_server_ffi.dylib > ../target/release/libpact_mock_server_ffi-osx-x86_64.dylib.gz
-gzip -c ../target/x86_64-apple-darwin/release/libpact_mock_server_ffi.a > ../target/release/libpact_mock_server_ffi-osx-x86_64.a.gz
+gzip -c ../target/release/libpact_mock_server_ffi.dylib > ../target/release/libpact_mock_server_ffi-osx-x86_64.dylib.gz
+gzip -c ../target/release/libpact_mock_server_ffi.a > ../target/release/libpact_mock_server_ffi-osx-x86_64.a.gz
--- rust/pact_mock_server_ffi/release-win.sh
+++ rust/pact_mock_server_ffi/release-win.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 cargo clean
-cargo build --release --target x86_64-pc-windows-gnu
-gzip -c ../target/x86_64-pc-windows-gnu/release/pact_mock_server_ffi.dll > ../target/release/libpact_mock_server_ffi-windows-x86_64.dll.gz
-gzip -c ../target/x86_64-pc-windows-gnu/release/pact_mock_server_ffi.lib > ../target/release/libpact_mock_server_ffi-windows-x86_64.lib.gz
+cargo build --release
+gzip -c ../target/release/pact_mock_server_ffi.dll > ../target/release/libpact_mock_server_ffi-windows-x86_64.dll.gz
+gzip -c ../target/release/pact_mock_server_ffi.lib > ../target/release/libpact_mock_server_ffi-windows-x86_64.lib.gz
