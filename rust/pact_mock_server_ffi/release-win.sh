90b93f1132c937a40151245b73e8ecf481a9c4c0
Ronald Holshausen <uglyog@gmail.com>
2020-05-01 14:45:49 +1000

    chore: create a conan package for the dll/so

--- rust/pact_mock_server_ffi/release-win.sh
+++ rust/pact_mock_server_ffi/release-win.sh
@@ -4,4 +4,5 @@ cargo clean
 cargo build --release
 mkdir -p ../target/artifacts
 gzip -c ../target/release/pact_mock_server_ffi.dll > ../target/artifacts/libpact_mock_server_ffi-windows-x86_64.dll.gz
+gzip -c ../target/release/pact_mock_server_ffi.dll.lib > ../target/artifacts/libpact_mock_server_ffi-windows-x86_64.dll.lib.gz
 gzip -c ../target/release/pact_mock_server_ffi.lib > ../target/artifacts/libpact_mock_server_ffi-windows-x86_64.lib.gz
