8443b29f785971d49f8f959c66004852eb597f98
Ronald Holshausen <rholshausen@dius.com.au>
2020-05-27 16:04:03 +1000

    chore: do not buuld the ios binary

--- rust/pact_mock_server_cli/release-osx.sh
+++ rust/pact_mock_server_cli/release-osx.sh
@@ -5,5 +5,5 @@ cargo clean
 mkdir -p ../target/artifacts
 cargo build --release
 gzip -c ../target/release/pact_mock_server_cli > ../target/artifacts/pact_mock_server_cli-osx-x86_64.gz
-cargo build --release --target x86_64-apple-ios
-gzip -c ../target/x86_64-apple-ios/release/pact_mock_server_cli > ../target/artifacts/pact_mock_server_cli-ios-x86_64.gz
+#cargo build --release --target x86_64-apple-ios
+#gzip -c ../target/x86_64-apple-ios/release/pact_mock_server_cli > ../target/artifacts/pact_mock_server_cli-ios-x86_64.gz
