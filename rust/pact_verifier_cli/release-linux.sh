f9cf35f3a315204ed8b6a8cf6a81933753872657
Ronald Holshausen <rholshausen@dius.com.au>
2021-01-05 15:05:43 +1100

    fix: correct the pact_verifier_cli release scripts

--- rust/pact_verifier_cli/release-linux.sh
+++ rust/pact_verifier_cli/release-linux.sh
@@ -5,5 +5,5 @@ cargo clean
 mkdir -p ../target/artifacts
 GENERATE_C_HEADER=true cargo build --release
 gzip -c ../target/release/pact_verifier_cli > ../target/artifacts/pact_verifier_cli-linux-x86_64.gz
-gzip -c ../target/release/pact_verifier_cli.so > ../target/artifacts/pact_verifier_cli-linux-x86_64.so.gz
-gzip -c ../target/release/pact_verifier_cli.a > ../target/artifacts/pact_verifier_cli-linux-x86_64.a.gz
+gzip -c ../target/release/libpact_verifier_cli.so > ../target/artifacts/libpact_verifier_cli-linux-x86_64.so.gz
+gzip -c ../target/release/libpact_verifier_cli.a > ../target/artifacts/libpact_verifier_cli-linux-x86_64.a.gz
--- rust/pact_verifier_cli/release-win.sh
+++ rust/pact_verifier_cli/release-win.sh
@@ -3,6 +3,6 @@
 mkdir -p ../target/artifacts
 cargo build --release
 gzip -c ../target/release/pact_verifier_cli.exe > ../target/artifacts/pact_verifier_cli-windows-x86_64.exe.gz
-gzip -c ../target/release/pact_verifier_cli.dll > ../target/artifacts/pact_verifier_cli-windows-x86_64.dll.gz
-gzip -c ../target/release/pact_verifier_cli.dll.lib > ../target/artifacts/pact_verifier_cli-windows-x86_64.dll.lib.gz
-gzip -c ../target/release/pact_verifier_cli.lib > ../target/artifacts/pact_verifier_cli-windows-x86_64.lib.gz
+gzip -c ../target/release/libpact_verifier_cli.dll > ../target/artifacts/libpact_verifier_cli-windows-x86_64.dll.gz
+gzip -c ../target/release/libpact_verifier_cli.dll.lib > ../target/artifacts/libpact_verifier_cli-windows-x86_64.dll.lib.gz
+gzip -c ../target/release/libpact_verifier_cli.lib > ../target/artifacts/libpact_verifier_cli-windows-x86_64.lib.gz
