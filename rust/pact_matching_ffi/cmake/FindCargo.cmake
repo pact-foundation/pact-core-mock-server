104782e6a9d3596ce8b3b9b277f5e9fb4698118c
Andrew Lilley Brinker <abrinker@mitre.org>
2021-02-02 10:16:12 -0800

    Added logic to detect when not using Cargo nightly (#56)

--- rust/pact_matching_ffi/cmake/FindCargo.cmake
+++ rust/pact_matching_ffi/cmake/FindCargo.cmake
@@ -3,12 +3,21 @@
 # on the program search path
 find_program(CARGO_EXECUTABLE cargo PATHS "$ENV{HOME}/.cargo" PATH_SUFFIXES bin)
 
+set(CARGO_VERSION "")
+set(CARGO_CHANNEL "stable")
+
 # If we found it, see if we can get its version.
 if(CARGO_EXECUTABLE)
     execute_process(COMMAND ${CARGO_EXECUTABLE} -V OUTPUT_VARIABLE CARGO_VERSION_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
     
     if(CARGO_VERSION_OUTPUT MATCHES "cargo ([0-9]+\\.[0-9]+\\.[0-9]+).*")
         set(CARGO_VERSION ${CMAKE_MATCH_1})
+    endif()
+
+    execute_process(COMMAND ${CARGO_EXECUTABLE} -V OUTPUT_VARIABLE CARGO_CHANNEL_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    if(CARGO_CHANNEL_OUTPUT MATCHES "cargo [0-9]+\\.[0-9]+\\.[0-9]+-([a-zA-Z]*).*")
+        set(CARGO_CHANNEL ${CMAKE_MATCH_1})
     endif()
 endif()
 
@@ -17,4 +26,12 @@ mark_as_advanced(CARGO_EXECUTABLE)
 
 # Require that we find both the executable and the version. Otherwise error out.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Cargo REQUIRED_VARS CARGO_EXECUTABLE CARGO_VERSION VERSION_VAR CARGO_VERSION)
+find_package_handle_standard_args(
+    Cargo
+    REQUIRED_VARS
+        CARGO_VERSION
+        CARGO_CHANNEL
+        CARGO_EXECUTABLE
+    VERSION_VAR
+        CARGO_VERSION
+)
