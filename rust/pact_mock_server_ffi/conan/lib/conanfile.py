38c88342a5f4249c416249e2b48f449bb73d6151
Ronald Holshausen <rholshausen@dius.com.au>
2021-03-14 14:50:41 +1100

    bump version to 0.0.17

--- rust/pact_mock_server_ffi/conan/lib/conanfile.py
+++ rust/pact_mock_server_ffi/conan/lib/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, VisualStudioBuildEnvironment, CMake, tools
 
 class CbindgenTestConan(ConanFile):
     name = "pact_mock_server_ffi"
-    version = "0.0.16"
+    version = "0.0.17"
     description = "Pact/Rust FFI bindings"
     url = "https://github.com/pact-foundation/pact-reference"
     license = "MIT"
