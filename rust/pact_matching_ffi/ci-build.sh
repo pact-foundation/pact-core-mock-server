163859e7b97d40f64d13ff9a24686c83d45f273f
Ronald Holshausen <rholshausen@dius.com.au>
2021-05-03 15:51:33 +1000

    chore: debugging build

--- rust/pact_matching_ffi/ci-build.sh
+++ rust/pact_matching_ffi/ci-build.sh
@@ -6,6 +6,8 @@ cmake --version
 cargo +nightly --version
 cargo install cbindgen
 
+rm -rf ./include
+
 echo #####################################
 echo # Build library with CMake
 echo #####################################
@@ -14,6 +16,7 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build . -v
 
+ls -la ..
 ls -la ../../target/debug
 
 echo #####################################
