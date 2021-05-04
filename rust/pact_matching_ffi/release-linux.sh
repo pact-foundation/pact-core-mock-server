b406d0fb1c9f5fbf94f6b55a2bbce8dc673eb2fb
Ronald Holshausen <rholshausen@dius.com.au>
2021-05-04 11:45:55 +1000

    chore: correct release on windows

--- rust/pact_matching_ffi/release-linux.sh
+++ rust/pact_matching_ffi/release-linux.sh
@@ -9,6 +9,6 @@ rustup run nightly cbindgen \
   --output include/pact_matching.h
 cp include/pact_matching.h ../target/artifacts
 gzip -c ../target/release/libpact_matching_ffi.so > ../target/artifacts/libpact_matching_ffi-linux-x86_64.so.gz
-openssl dgst -sha256 -r ../target/artifacts/libpact_matching_ffi-linux-x86_64.a.gz > ../target/artifacts/libpact_matching_ffi-linux-x86_64.a.gz.sha256
+openssl dgst -sha256 -r ../target/artifacts/libpact_matching_ffi-linux-x86_64.so.gz > ../target/artifacts/libpact_matching_ffi-linux-x86_64.so.gz.sha256
 gzip -c ../target/release/libpact_matching_ffi.a > ../target/artifacts/libpact_matching_ffi-linux-x86_64.a.gz
 openssl dgst -sha256 -r ../target/artifacts/libpact_matching_ffi-linux-x86_64.a.gz > ../target/artifacts/libpact_matching_ffi-linux-x86_64.a.gz.sha256
--- rust/pact_matching_ffi/release-win.sh
+++ rust/pact_matching_ffi/release-win.sh
@@ -3,9 +3,9 @@
 cargo clean
 cargo build --release
 mkdir -p ../target/artifacts
-gzip -c ../target/release/libpact_matching_ffi.dll > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.gz
-openssl dgst -sha256 -r ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.gz > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.gz.sha256
-gzip -c ../target/release/libpact_matching_ffi.dll.lib > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.lib.gz
-openssl dgst -sha256 -r ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.lib.gz > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.dll.lib.gz.sha256
-gzip -c ../target/release/libpact_matching_ffi.lib > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.lib.gz
-openssl dgst -sha256 -r ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.lib.gz > ../target/artifacts/liblibpact_matching_ffi-windows-x86_64.lib.gz.sha256
+gzip -c ../target/release/pact_matching_ffi.dll > ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.gz
+openssl dgst -sha256 -r ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.gz > ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.gz.sha256
+gzip -c ../target/release/pact_matching_ffi.dll.lib > ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.lib.gz
+openssl dgst -sha256 -r ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.lib.gz > ../target/artifacts/pact_matching_ffi-windows-x86_64.dll.lib.gz.sha256
+gzip -c ../target/release/pact_matching_ffi.lib > ../target/artifacts/pact_matching_ffi-windows-x86_64.lib.gz
+openssl dgst -sha256 -r ../target/artifacts/pact_matching_ffi-windows-x86_64.lib.gz > ../target/artifacts/pact_matching_ffi-windows-x86_64.lib.gz.sha256
