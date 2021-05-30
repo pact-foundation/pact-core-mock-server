13221ad98739b024927edaf00045b5fea5b69f0e
Ronald Holshausen <rholshausen@dius.com.au>
2021-05-30 18:30:44 +1000

    chore: Mock server CLI release build was overwriting the Windows exe with the SHA checksum

--- rust/pact_mock_server_cli/release-win.sh
+++ rust/pact_mock_server_cli/release-win.sh
@@ -4,4 +4,4 @@ cargo clean
 mkdir -p ../target/artifacts
 cargo build --release
 gzip -c ../target/release/pact_mock_server_cli.exe > ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz
-openssl dgst -sha256 -r ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz > ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz
+openssl dgst -sha256 -r ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz > ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz.sha256
