372cbdd17029baa4402e56dd8728c587cc805d26
Ronald Holshausen <rholshausen@dius.com.au>
2021-07-18 15:57:30 +1000

    chore: remove CMake step from CI build

--- rust/pact_ffi/ci-build.sh
+++ rust/pact_ffi/ci-build.sh
@@ -7,14 +7,14 @@ which cargo
 cargo install cbindgen
 rm -rf ./include
 
-echo -------------------------------------
-echo - Build library with CMake
-echo -------------------------------------
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . -v
-cd ..
+#echo -------------------------------------
+#echo - Build library with CMake
+#echo -------------------------------------
+#mkdir -p build
+#cd build
+#cmake -DCMAKE_BUILD_TYPE=Debug ..
+#cmake --build . -v
+#cd ..
 
 echo -------------------------------------
 echo - Generate header with cbindgen
@@ -24,23 +24,23 @@ rustup run nightly cbindgen \
   --crate pact_ffi \
   --output include/pact.h
 
-echo -------------------------------------
-echo - Make library available for examples
-echo -------------------------------------
-cd build
-cmake --install . --prefix ./install
-
-echo -------------------------------------
-echo - Running examples
-echo -------------------------------------
-cd ..
-for i in examples/*; do
-  pushd $i
-  mkdir -p build
-  cd build
-  cmake ..
-  cmake --build .
-  echo Running example
-  ./example
-  popd
-done
+#echo -------------------------------------
+#echo - Make library available for examples
+#echo -------------------------------------
+#cd build
+#cmake --install . --prefix ./install
+#
+#echo -------------------------------------
+#echo - Running examples
+#echo -------------------------------------
+#cd ..
+#for i in examples/*; do
+#  pushd $i
+#  mkdir -p build
+#  cd build
+#  cmake ..
+#  cmake --build .
+#  echo Running example
+#  ./example
+#  popd
+#done
