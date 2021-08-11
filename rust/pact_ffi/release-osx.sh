a99403257f64b984196f495c4494afcf01d78f4e
Matt Fellows <matt.fellows@onegeek.com.au>
2021-08-11 22:57:46 +1000

    chore: release m1 arm package for new Mac hardware

--- rust/pact_ffi/release-osx.sh
+++ rust/pact_ffi/release-osx.sh
@@ -7,3 +7,14 @@ gzip -c ../target/release/libpact_ffi.dylib > ../target/artifacts/libpact_ffi-os
 openssl dgst -sha256 -r ../target/artifacts/libpact_ffi-osx-x86_64.dylib.gz > ../target/artifacts/libpact_ffi-osx-x86_64.dylib.gz.sha256
 gzip -c ../target/release/libpact_ffi.a > ../target/artifacts/libpact_ffi-osx-x86_64.a.gz
 openssl dgst -sha256 -r ../target/artifacts/libpact_ffi-osx-x86_64.a.gz > ../target/artifacts/libpact_ffi-osx-x86_64.a.gz.sha256
+
+# M1
+cargo clean
+export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
+export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
+cargo build --target aarch64-apple-darwin --release
+
+gzip -c ../target/aarch64-apple-darwin/release/libpact_ffi.dylib > ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.dylib.gz
+openssl dgst -sha256 -r ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.dylib.gz > ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.dylib.gz.sha256
+gzip -c ../target/aarch64-apple-darwin/release/libpact_ffi.a > ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.a.gz
+openssl dgst -sha256 -r ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.a.gz > ../target/artifacts/libpact_ffi-osx-aarch64-apple-darwin.a.gz.sha256
