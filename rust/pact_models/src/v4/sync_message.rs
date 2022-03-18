01ac989b3cab8383a144d36b7fe86c2f3f8e983c
Ronald Holshausen <rholshausen@dius.com.au>
2022-03-18 16:04:00 +1100

    fix: was missing setter to set the transport with V4 interactions

--- rust/pact_models/src/v4/sync_message.rs
+++ rust/pact_models/src/v4/sync_message.rs
@@ -238,6 +238,10 @@ impl V4Interaction for SynchronousMessage {
   fn transport(&self) -> Option<String> {
     self.transport.clone()
   }
+
+  fn set_transport(&mut self, transport: Option<String>) {
+    self.transport = transport.clone();
+  }
 }
 
 impl Interaction for SynchronousMessage {
