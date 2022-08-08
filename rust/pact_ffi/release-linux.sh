12ce4f6c3c039f410eaa9298d3e4c6ba27f675d3
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-08-08 13:35:26 +1000

    chore: missed some references to nightly-2022-04-12

--- rust/pact_ffi/release-linux.sh
+++ rust/pact_ffi/release-linux.sh
@@ -12,13 +12,13 @@ gzip -c ../target/release/libpact_ffi.a > ../target/artifacts/libpact_ffi-linux-
 openssl dgst -sha256 -r ../target/artifacts/libpact_ffi-linux-x86_64.a.gz > ../target/artifacts/libpact_ffi-linux-x86_64.a.gz.sha256
 
 echo -- Generate the header files --
-rustup toolchain install nightly-2022-04-12
-rustup component add rustfmt --toolchain nightly-2022-04-12-x86_64-unknown-linux-gnu
-rustup run nightly-2022-04-12 cbindgen \
+rustup toolchain install nightly
+rustup component add rustfmt --toolchain nightly
+rustup run nightly cbindgen \
   --config cbindgen.toml \
   --crate pact_ffi \
   --output include/pact.h
-rustup run nightly-2022-04-12 cbindgen \
+rustup run nightly cbindgen \
   --config cbindgen-c++.toml \
   --crate pact_ffi \
   --output include/pact-cpp.h
