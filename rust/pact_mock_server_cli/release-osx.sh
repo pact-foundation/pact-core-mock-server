a7349c74ac1d44c6ece0e961a5abbdb011de81cc
Yousaf Nabi <you@saf.dev>
2023-04-12 12:29:14 +0100

    feat: pact_mock_server_cli aarch64 osx

--- rust/pact_mock_server_cli/release-osx.sh
+++ rust/pact_mock_server_cli/release-osx.sh
@@ -8,3 +8,12 @@ gzip -c ../target/release/pact_mock_server_cli > ../target/artifacts/pact_mock_s
 #cargo build --release --target x86_64-apple-ios
 #gzip -c ../target/x86_64-apple-ios/release/pact_mock_server_cli > ../target/artifacts/pact_mock_server_cli-ios-x86_64.gz
 openssl dgst -sha256 -r ../target/artifacts/pact_mock_server_cli-osx-x86_64.gz > ../target/artifacts/pact_mock_server_cli-osx-x86_64.gz.sha256
+
+
+# M1
+export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
+export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
+cargo build --target aarch64-apple-darwin --release
+
+gzip -c ../target/aarch64-apple-darwin/release/pact_mock_server_cli > ../target/artifacts/pact_mock_server_cli-osx-aarch64.gz
+openssl dgst -sha256 -r ../target/artifacts/pact_mock_server_cli-osx-aarch64.gz > ../target/artifacts/pact_mock_server_cli-osx-aarch64.gz.sha256
