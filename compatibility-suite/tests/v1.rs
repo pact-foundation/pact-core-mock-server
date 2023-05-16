74c1d6de96b23929cce7979775c8dc0c89c538fa
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-05-16 14:05:22 +1000

    fix: fail compatibility suite on unimplemented steps

--- compatibility-suite/tests/v1.rs
+++ compatibility-suite/tests/v1.rs
@@ -6,5 +6,8 @@ pub struct PactWorld {
 }
 
 fn main() {
-  futures::executor::block_on(PactWorld::run("pact-compatibility-suite/features/V1"));
+  futures::executor::block_on(
+    PactWorld::cucumber()
+        .fail_on_skipped()
+        .run_and_exit("pact-compatibility-suite/features/V1"));
 }
