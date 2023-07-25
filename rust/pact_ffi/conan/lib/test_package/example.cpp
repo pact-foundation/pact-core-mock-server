8a5ef9ee219171d069813822c0097f8a1b646162
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-07-26 09:30:11 +1000

    Squashed 'compatibility-suite/pact-compatibility-suite/' changes from ff1d71f8..f3fb1e2f
    f3fb1e2f fix: correct V1 error message, which was not consistant with V3
    
    git-subtree-dir: compatibility-suite/pact-compatibility-suite
    git-subtree-split: f3fb1e2f3619f60cc26f12876e4a2e7b03c7d9c3

--- rust/pact_ffi/conan/lib/test_package/example.cpp
+++
@@ -1,7 +0,0 @@
-#include <pact-cpp.h>
-
-int main(int argc, char *argv[]) {
-    int port = pactffi_create_mock_server("{}", "127.0.0.1:0", false);
-    pactffi_mock_server_matched(port);
-    return 0;
-}
--- rust/pact_mock_server_cli/tests/cli-tests.rs
+++
@@ -1,8 +0,0 @@
-#[test]
-#[cfg(not(windows))]
-fn cli_tests() {
-  trycmd::TestCases::new()
-    .case("tests/cmd/*.toml")
-    .insert_var("[CLIVERSION]", clap::crate_version!()).unwrap()
-    .case("README.md");
-}
