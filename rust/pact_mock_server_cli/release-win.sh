6d3883e17230fe5fc299ffe533cb4c496c946e2f
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-07-31 15:16:10 +1000

    Squashed 'compatibility-suite/pact-compatibility-suite/' changes from 89b61f02..ad77df02
    ad77df02 fix: Update error steps to support regexes due to optional spaces
    
    git-subtree-dir: compatibility-suite/pact-compatibility-suite
    git-subtree-split: ad77df02b27cb0e139d59b33d18015a5d99c244f

--- rust/pact_mock_server_cli/release-win.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cargo clean
-mkdir -p ../target/artifacts
-cargo build --release
-gzip -c ../target/release/pact_mock_server_cli.exe > ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz
-openssl dgst -sha256 -r ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz > ../target/artifacts/pact_mock_server_cli-windows-x86_64.exe.gz.sha256
