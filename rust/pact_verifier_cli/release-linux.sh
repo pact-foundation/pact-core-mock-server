1b847777cdd01017427fb51358e69d16a5870145
Yousaf Nabi <you@saf.dev>
2024-01-24 11:54:22 +0000

    fix: pin cross to 0.2.5 for glibc 2.23

--- rust/pact_verifier_cli/release-linux.sh
+++ rust/pact_verifier_cli/release-linux.sh
@@ -9,7 +9,8 @@ openssl dgst -sha256 -r ../target/artifacts/pact_verifier_cli-linux-x86_64.gz > 
 
 # aarch64 is failing to build on Rust 1.70+, and the dependencies need Rust 1.70+
 #echo -- Build the aarch64 release artifacts --
-#cargo install cross
+
+#cargo install cross@0.2.5
 #cross build --target aarch64-unknown-linux-gnu --release
 #gzip -c ../target/aarch64-unknown-linux-gnu/release/pact_verifier_cli > ../target/artifacts/pact_verifier_cli-linux-aarch64.gz
 #openssl dgst -sha256 -r ../target/artifacts/pact_verifier_cli-linux-aarch64.gz > ../target/artifacts/pact_verifier_cli-linux-aarch64.gz.sha256
