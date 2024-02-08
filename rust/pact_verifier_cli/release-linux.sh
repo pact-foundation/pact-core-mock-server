9196efe4d9e62e87f3258a590000c63831e258b3
Yousaf Nabi <you@saf.dev>
2024-02-08 20:36:10 +0000

    feat: verifier/mock_server clis - build for musl

--- rust/pact_verifier_cli/release-linux.sh
+++ rust/pact_verifier_cli/release-linux.sh
@@ -14,3 +14,16 @@ cargo clean
 cross build --target aarch64-unknown-linux-gnu --release
 gzip -c ../target/aarch64-unknown-linux-gnu/release/pact_verifier_cli > ../release_artifacts/pact_verifier_cli-linux-aarch64.gz
 openssl dgst -sha256 -r ../release_artifacts/pact_verifier_cli-linux-aarch64.gz > ../release_artifacts/pact_verifier_cli-linux-aarch64.gz.sha256
+
+echo -- Build the musl release artifacts --
+sudo apt install musl-tools
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --target=x86_64-unknown-linux-musl
+gzip -c ../target/x86_64-unknown-linux-musl/release/pact_verifier_cli > ../release_artifacts/pact_verifier_cli-linux-x86_64-musl.gz
+openssl dgst -sha256 -r ../release_artifacts/pact_verifier_cli-linux-x86_64-musl.gz > ../release_artifacts/pact_verifier_cli-linux-x86_64-musl.gz.sha256
+
+echo -- Build the musl aarch64 release artifacts --
+cargo clean
+cross build --release --target=aarch64-unknown-linux-musl
+gzip -c ../target/aarch64-unknown-linux-gnu/release/pact_verifier_cli > ../release_artifacts/pact_verifier_cli-linux-aarch64-musl.gz
+openssl dgst -sha256 -r ../release_artifacts/pact_verifier_cli-linux-aarch64-musl.gz > ../release_artifacts/pact_verifier_cli-linux-aarch64-musl.gz.sha256
