7732a637b0e890b9ecb29470a03908d8f30a523a
JP-Ellis <josh@jpellis.me>
2024-02-26 17:54:18 +1100

    chore(ci): refactor release pipeline
    This is a refactor of the release pipeline scripts and includes a number
    of changes:
    
    - Scripts have been reorganised into independent functions for each
      target being built. This should, hopefully, help maintain these
      scripts going forward.
    
    - The scripts allow for arguments to be passed directly to `cargo`,
      thereby allowing for non-release builds as well.
    
    - The release pipeline now also runs on PRs. If being run on PRs, a
      non-release build done as it is faster.
    
    - The publishing steps of the pipeline have been separated into a
      distinct job (which only runs on releases)
    
    - A concurrency group is set up, which will cancel existing builds if a
      new build is triggered on the same PR or on master.
    
    - Some actions in the pipeline have been updated.
    
    - The macOS SDK target has been upgraded to version 12 as version 11
      is no longer maintained.
    
    - Cache dependencies to try and reduce the runtime
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- rust/pact_verifier_cli/release-osx.sh
+++ rust/pact_verifier_cli/release-osx.sh
@@ -1,14 +1,42 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-mkdir -p ../release_artifacts
-cargo build --release
-gzip -c ../target/release/pact_verifier_cli > ../release_artifacts/pact_verifier_cli-osx-x86_64.gz
-openssl dgst -sha256 -r ../release_artifacts/pact_verifier_cli-osx-x86_64.gz > ../release_artifacts/pact_verifier_cli-osx-x86_64.gz.sha256
+set -e
+set -x
 
-# M1
-export SDKROOT=$(xcrun -sdk macosx11.1 --show-sdk-path)
-export MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.1 --show-sdk-platform-version)
-cargo build --target aarch64-apple-darwin --release
+RUST_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
 
-gzip -c ../target/aarch64-apple-darwin/release/pact_verifier_cli > ../release_artifacts/pact_verifier_cli-osx-aarch64.gz
-openssl dgst -sha256 -r ../release_artifacts/pact_verifier_cli-osx-aarch64.gz > ../release_artifacts/pact_verifier_cli-osx-aarch64.gz.sha256
+source "$RUST_DIR/scripts/gzip-and-sum.sh"
+ARTIFACTS_DIR=${ARTIFACTS_DIR:-"$RUST_DIR/release_artifacts"}
+mkdir -p "$ARTIFACTS_DIR"
+export CARGO_TARGET_DIR=${CARO_TARGET_DIR:-"$RUST_DIR/target"}
+
+# We target the oldest supported version of macOS.
+export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-12}
+
+# All flags passed to this script are passed to cargo.
+cargo_flags=( "$@" )
+
+# Build the x86_64 darwin release
+build_x86_64() {
+    cargo build --target x86_64-apple-darwin "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR/x86_64-apple-darwin/release/pact_verifier_cli" \
+            "$ARTIFACTS_DIR/pact_verifier_cli-osx-x86_64.gz"
+    fi
+}
+
+# Build the aarch64 darwin release
+build_aarch64() {
+    cargo build --target aarch64-apple-darwin "${cargo_flags[@]}"
+
+    if [[ "${cargo_flags[*]}" =~ "--release" ]]; then
+        gzip_and_sum \
+            "$CARGO_TARGET_DIR//aarch64-apple-darwin/release/pact_verifier_cli" \
+            "$ARTIFACTS_DIR/pact_verifier_cli-osx-aarch64.gz"
+    fi
+}
+
+build_x86_64
+build_aarch64
