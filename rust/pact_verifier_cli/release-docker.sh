e08dc26046059ddcd179989df82b757648443a24
Ronald Holshausen <ronald.holshausen@smartbear.com>
2024-03-18 11:27:24 +1100

    chore(pact_verifier_cli): Create multi-arch docker images

--- rust/pact_verifier_cli/release-docker.sh
+++ rust/pact_verifier_cli/release-docker.sh
@@ -8,7 +8,22 @@ if [ "" = "$1" ]; then
   exit 1
 fi
 
-docker build . -t pactfoundation/pact-ref-verifier:$1
-docker push pactfoundation/pact-ref-verifier:$1
-docker tag pactfoundation/pact-ref-verifier:$1 pactfoundation/pact-ref-verifier:latest
-docker push pactfoundation/pact-ref-verifier:latest
+# AMD64
+docker build . -t pactfoundation/pact-ref-verifier:$1-amd64 --platform linux/amd64 \
+    --build-arg ARCH=amd64/ --build-arg BIN_ARCH=x86_64 --build-arg VERSION=$1
+docker push pactfoundation/pact-ref-verifier:$1-amd64
+
+# ARM64V8
+docker build . -t pactfoundation/pact-ref-verifier:$1-arm64v8 --platform linux/arm64 \
+    --build-arg ARCH=arm64v8/ --build-arg BIN_ARCH=aarch64 --build-arg VERSION=$1
+docker push pactfoundation/pact-ref-verifier:$1-arm64v8
+
+# Create Manifest
+docker manifest create pactfoundation/pact-ref-verifier:$1 \
+    --amend pactfoundation/pact-ref-verifier:$1-amd64 \
+    --amend pactfoundation/pact-ref-verifier:$1-arm64v8
+docker manifest push pactfoundation/pact-ref-verifier:$1
+docker manifest create pactfoundation/pact-ref-verifier:latest \
+    --amend pactfoundation/pact-ref-verifier:$1-amd64 \
+    --amend pactfoundation/pact-ref-verifier:$1-arm64v8
+docker manifest push pactfoundation/pact-ref-verifier:latest
