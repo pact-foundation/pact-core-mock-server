18ea437aefcd0d5bf8df4ef48f8b9a7205a29af3
Ronald Holshausen <ronald.holshausen@smartbear.com>
2024-06-07 16:34:21 +1000

    chore: Skip IP6 in tests when running within docker

--- pact_mock_server/src/builder.rs
+++ pact_mock_server/src/builder.rs
@@ -113,45 +113,47 @@ mod tests {
 
   #[test_log::test]
   fn basic_mock_server_test() {
-    let pact = V4Pact {
-      interactions: vec![
-        SynchronousHttp {
-          request: HttpRequest {
-            headers: Some(hashmap! {
-            "accept".to_string() => vec!["application/json".to_string()]
-          }),
-            .. HttpRequest::default()
-          },
-          .. SynchronousHttp::default()
-        }.boxed_v4()
-      ],
-      .. V4Pact::default()
-    };
+    if(!env::var("NO_IP6").is_ok()) {
+      let pact = V4Pact {
+        interactions: vec![
+          SynchronousHttp {
+            request: HttpRequest {
+              headers: Some(hashmap! {
+              "accept".to_string() => vec!["application/json".to_string()]
+            }),
+              ..HttpRequest::default()
+            },
+            ..SynchronousHttp::default()
+          }.boxed_v4()
+        ],
+        ..V4Pact::default()
+      };
 
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-      .enable_all()
-      .build()
-      .unwrap();
+      let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
 
-    let mock_server = runtime.block_on(async {
-      MockServerBuilder::new()
-        .with_v4_pact(pact)
-        .start()
-        .await
-        .unwrap()
-    });
+      let mock_server = runtime.block_on(async {
+        MockServerBuilder::new()
+          .with_v4_pact(pact)
+          .start()
+          .await
+          .unwrap()
+      });
 
-    let client = reqwest::blocking::Client::new();
-    let response = client.get(format!("http://[::1]:{}", mock_server.port()).as_str())
-      .header(ACCEPT, "application/json").send();
+      let client = reqwest::blocking::Client::new();
+      let response = client.get(format!("http://[::1]:{}", mock_server.port()).as_str())
+        .header(ACCEPT, "application/json").send();
 
-    mock_server.shutdown().unwrap();
-    let all_matched = mock_server.all_matched();
-    let mismatches = mock_server.mismatches();
+      mock_server.shutdown().unwrap();
+      let all_matched = mock_server.all_matched();
+      let mismatches = mock_server.mismatches();
 
-    expect!(response.unwrap().status()).to(be_equal_to(200));
-    expect!(all_matched).to(be_true());
-    expect!(mismatches).to(be_equal_to(vec![]));
+      expect!(response.unwrap().status()).to(be_equal_to(200));
+      expect!(all_matched).to(be_true());
+      expect!(mismatches).to(be_equal_to(vec![]));
+    }
   }
 
   #[test_log::test]
